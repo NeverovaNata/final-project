94999c87e990a6745341444f42567c7c551475b3
Natalia <neverovanatalja@yandex.ru>
2025-03-18 23:35:42 +0300

    Работа над ошибками. Добавлена работа с pytest и скриншот выполнения теста

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-APP_URL = "https://ed3d48e2-9beb-472e-85ba-d5b824b9012d.serverhub.praktikum-services.ru/"
+APP_URL = "https://837a87e5-e948-4b1a-aee1-456bbaf0ff9f.serverhub.praktikum-services.ru/"
 COURIER_CREATE = "api/v1/courier"
 ORDER_CREATE = "api/v1/orders"
 ORDER_CHECK = "api/v1/orders/track?t="
--- test_order.py
+++ test_order.py
@@ -6,7 +6,7 @@ import requests
 courier_create_url = settings.APP_URL + settings.COURIER_CREATE
 take_order_url = settings.APP_URL + settings.ORDER_CHECK
 
-def test_courier_login(url):
+def check_courier_login(url):
     answer = courier.create_courier(url)
     if "ok" in answer:
         print("Курьер создан")
@@ -14,11 +14,8 @@ def test_courier_login(url):
         print("Курьер уже был создан ранее")
 
 def test_created_order_by_track():
-    test_courier_login(courier_create_url)
+    check_courier_login(courier_create_url)
     track = orders.create_order()['track']
     url = take_order_url + str(track)
     pull_order = requests.get(url)
     assert pull_order.status_code == 200, f"Expected status code 200, but got {pull_order.status_code}"
-    print("Test - OK")
-
-test_created_order_by_track()
